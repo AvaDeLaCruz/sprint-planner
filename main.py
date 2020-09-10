b998cd56b333e0007643a8624f7c9c11a2f66a88
Ava DeLaCruz <avadelac@usc.edu>
2020-09-10 15:33:34 -0400

    starting dev

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from queue import PriorityQueue
+
+
+def main():
+    return 1
+
+
+if __name__ == '__main__':
+    main()
+    # Write your code here
