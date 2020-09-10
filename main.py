46a6cea6b203fa2fd8e8d1e777240dddbc7579fc
Ava DeLaCruz <avadelac@usc.edu>
2020-09-10 18:29:43 -0400

    reading numTeams, numSprints, numTickets done

--- main.py
+++ main.py
@@ -7,8 +7,37 @@ import re
 import sys
 from queue import PriorityQueue
 
+# i'm using this challenge as an opportunity to get more practice with python (i'm pretty new to it!) so i'll be linking the resources i used for future reference in the comments
+
+
+def writeResults():
+    return None
+
+
+def assignTickets(numTeams, numSprints, numTickets, ticketList, ticketPQ):
+    return None
+
+
+def parseAndStoreTicketData():
+    return [], None
+
+
+def parseNumberInputs():
+    # https://www.journaldev.com/32137/read-stdin-python
+    numTeams = input()
+    numSprints = input()
+    numTickets = input()
+    print(numTeams, numSprints, numTickets)
+    return 1, 2, 3
+
 
 def main():
+    # https://realpython.com/python-pass-by-reference/
+    # instead of passing vars by reference, return multiple vars
+    numTeams, numSprints, numTickets = parseNumberInputs()
+    # ticketList, ticketPQ = parseAndStoreTicketData()
+    # assignTickets(numTeams, numSprints, numTickets, ticketList, ticketPQ)
+    # writeResults()
     return 1
 
 
