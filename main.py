ac8b94d4f61bd2b146c9e8a9ddca84a4ee14b2aa
Ava DeLaCruz <avadelac@usc.edu>
2020-09-10 20:44:34 -0400

    add Ticket class, constructor, and assignSprintID method

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ from queue import PriorityQueue
 
 # i'm using this challenge as an opportunity to get more practice with python (i'm pretty new to it!) so i'll be linking the resources i used for future reference in the comments
 
+# https://www.w3schools.com/python/python_classes.asp
+# https://www.programiz.com/python-programming/class
+
+
+class Ticket:
+    def __init__(self, ticketID, description, teamID, priority):
+        self.ticketID = ticketID
+        self.description = description
+        self.teamID = teamID
+        self.priority = priority
+        self.sprintID = None
+
+    def assignSprintID(self, sprintID):
+        self.sprintID = sprintID
+
 
 def writeResults():
     return None
@@ -34,7 +49,13 @@ def main():
     # https://realpython.com/python-pass-by-reference/
     # instead of passing vars by reference, return multiple vars
     numTeams, numSprints, numTickets = parseNumberInputs()
-    # ticketList, ticketPQ = parseAndStoreTicketData()
+
+    # test = Ticket(1, 'hey there', 4, 'HIGH')
+    # https://stackoverflow.com/questions/192109/is-there-a-built-in-function-to-print-all-the-current-properties-and-values-of-a
+    # print current attributes and values of obj
+    # print(test.__dict__)
+
+    ticketList, ticketPQ = parseAndStoreTicketData()
     # assignTickets(numTeams, numSprints, numTickets, ticketList, ticketPQ)
     # writeResults()
     return 1
