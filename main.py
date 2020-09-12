7cdf28d7d64ec2d8877b9a8d034fa89a8bb57f9e
Ava DeLaCruz <avadelac@usc.edu>
2020-09-12 00:18:24 -0400

    final cleanup done

--- main.py
+++ main.py
@@ -7,22 +7,12 @@ import re
 import sys
 from queue import PriorityQueue
 
-# i'm using this challenge as an opportunity to get more practice with python (i'm pretty new to it!) so i'll be linking the resources i used for future reference in the comments
-
-# https://www.w3schools.com/python/python_classes.asp
-# https://www.programiz.com/python-programming/class
-
-
-# class TicketMetaclass:
-#     def __repr__(self):
-#         return "hi"
+# i'm using this challenge as an opportunity to get more practice with python
+# (i'm pretty new to it!) so i'll be linking the resources i used for future
+# reference in the comments at the bottom of the file
 
 
 class Ticket:
-
-    # __metaclass__ = TicketMetaclass
-    # https://www.geeksforgeeks.org/switch-case-in-python-replacement/
-    # https://towardsdatascience.com/introduction-to-priority-queues-in-python-83664d3178c3
     priorities = {
         "HIGH": 0,
         "MED": 1,
@@ -40,7 +30,6 @@ class Ticket:
     def assignSprintID(self, sprintID):
         self.sprintID = sprintID
 
-# https://dbader.org/blog/python-repr-vs-str
     def __str__(self):
         ticketString = str(self.ticketID) + "/ " + self.description + \
             "/ " + str(self.teamID) + "/ " + self.priority
@@ -60,6 +49,7 @@ def writeResults(assignedTickets, ticketList, numTickets):
                          for i in range(numTickets) if i not in assignedTicketIDs]
     for ticket in unassignedTickets:
         print(ticket)
+
     return None
 
 
@@ -68,7 +58,6 @@ def assignTicketsToSprints(numSprints, teamsTicketsList):
     for sprintNum in range(numSprints):
         for teamsTickets in teamsTicketsList:
             if not teamsTickets.empty():
-                # oldPriority, ticket = teamsTickets.get()
                 teamTuple = teamsTickets.get()
                 ticket = teamTuple[1]
                 ticket.assignSprintID(sprintNum)
@@ -83,7 +72,6 @@ def sortTicketsByTeam(numTeams, numTickets, ticketList):
     for teamNumber in range(numTeams):
         teamPQ = PriorityQueue()
         teamsTicketsList.insert(teamNumber, teamPQ)
-    # for n in range(numTickets):
     for ticket in ticketList:
         # create an entry with priorityValue as the first priority
         # ticketID as the tiebreaker
@@ -96,22 +84,10 @@ def sortTicketsByTeam(numTeams, numTickets, ticketList):
         teamPQ = teamsTicketsList[teamID]
         teamPQ.put(entry)
 
-    # i = 0
-# https://www.bogotobogo.com/python/python_PriorityQueue_heapq_Data_Structure.php
-    # for teamsTickets in teamsTicketsList:
-    #     print(i)
-    #     while not teamsTickets.empty():
-    #         priority, ticket = teamsTickets.get()
-    #         print(ticket.__dict__)
-    #     i = i+1
-
     return teamsTicketsList
 
 
 def parseTicketData(numTickets):
-    # https://stackoverflow.com/questions/8162021/analyzing-string-input-until-it-reaches-a-certain-letter-on-python
-    # https://docs.python.org/2/library/stdtypes.html#str.partition
-    # https://www.w3schools.com/python/python_for_loops.asp
     ticketList = []
     for line in sys.stdin:
         ticketData = []
@@ -127,49 +103,44 @@ def parseTicketData(numTickets):
             ticketData[2]), ticketData[3].rstrip())
         ticketList.append(ticket)
 
-    # https://docs.python.org/3.1/library/heapq.html#priority-queue-implementation-notes
-
     return ticketList
 
 
 def parseNumberInputs():
-    # https://www.journaldev.com/32137/read-stdin-python
-    # https://realpython.com/convert-python-string-to-int/
     numTeams = int(input())
     numSprints = int(input())
     numTickets = int(input())
+
     return numTeams, numSprints, numTickets
 
 
 def main():
-
-    # https://realpython.com/python-pass-by-reference/
-    # instead of passing vars by reference, return multiple vars
     numTeams, numSprints, numTickets = parseNumberInputs()
-
-    # https://stackoverflow.com/questions/192109/is-there-a-built-in-function-to-print-all-the-current-properties-and-values-of-a
-    # print current attributes and values of obj
-    # test = Ticket(1, 'hey there', 4, 'HIGH')
-    # print(test.__dict__)
-    # test.assignSprintID(9)
-    # print(test.__dict__)
-
     ticketList = parseTicketData(numTickets)
-
     teamsTicketsList = sortTicketsByTeam(numTeams, numTickets, ticketList)
-
     assignedTickets = assignTicketsToSprints(numSprints, teamsTicketsList)
-
     writeResults(assignedTickets, ticketList, numTickets)
 
-    # for ticket in ticketList:
-    #     print(ticket.__dict__)
-
-    # assignTickets(numTeams, numSprints, numTickets, ticketList, ticketPQ)
-    # writeResults()
     return 1
 
 
 if __name__ == '__main__':
     main()
-    # Write your code here
+
+
+# learnings and resources
+# https://stackoverflow.com/questions/192109/is-there-a-built-in-function-to-print-all-the-current-properties-and-values-of-a
+# https://realpython.com/python-pass-by-reference/
+# instead of passing vars by reference, return multiple vars
+# https://www.journaldev.com/32137/read-stdin-python
+# https://realpython.com/convert-python-string-to-int/
+# https://docs.python.org/3.1/library/heapq.html#priority-queue-implementation-notes
+# https://stackoverflow.com/questions/8162021/analyzing-string-input-until-it-reaches-a-certain-letter-on-python
+# https://docs.python.org/2/library/stdtypes.html#str.partition
+# https://www.w3schools.com/python/python_for_loops.asp
+# https://www.bogotobogo.com/python/python_PriorityQueue_heapq_Data_Structure.php
+# https://dbader.org/blog/python-repr-vs-str
+# https://www.geeksforgeeks.org/switch-case-in-python-replacement/
+# https://towardsdatascience.com/introduction-to-priority-queues-in-python-83664d3178c3
+# https://www.w3schools.com/python/python_classes.asp
+# https://www.programiz.com/python-programming/class
